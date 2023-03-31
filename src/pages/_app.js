3ef3df054f7ed9d758969adad2e6db7f3eb5f5d8
abraham <abraham.turnsek@gmail.com>
2023-03-31 13:20:11 +0200

    añadido de gtm

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 //import Layout from "@/componentes/Layout/Layout";
 
 export default function App({ Component, pageProps }) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM || "";
+  const tagManagerArgs = {
+    gtmId,
+  };
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
   return <Component {...pageProps} />;
 }
