c773a6edb4a0e2f7de67627e5fbed5aff766d917
abraham <abraham.turnsek@gmail.com>
2023-04-18 13:56:07 +0200

    correccion del error del menu mobil

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,22 +6,6 @@ export default function Document() {
     <Html lang="es">
       <script
         defer
-        id="Cookiebot"
-        src="https://consent.cookiebot.com/uc.js"
-        data-cbid="66a968ad-70de-4703-81be-84844ed28395"
-        data-blockingmode="auto"
-        type="text/javascript"
-        // strategy="lazyOnload"
-      ></script>
-      <script
-        defer
-        id="CookieDeclaration"
-        src="https://consent.cookiebot.com/66a968ad-70de-4703-81be-84844ed28395/cd.js"
-        type="text/javascript"
-        //strategy="lazyOnload"
-      ></script>
-      <script
-        defer
         //property="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
